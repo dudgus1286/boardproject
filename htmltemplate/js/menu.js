e4222b9ec0921010027650acf6f39ce3139c361b
dudgus1286 <dudgus1286@gmail.com>
2024-05-16 18:20:02 +0900

    페이지 상단부분, 게시글 작성 페이지, 멤버 로그인, 가입, 정보수정 페이지 디자인 기본틀

--- htmltemplate/js/menu.js
+++ htmltemplate/js/menu.js
@@ -1,4 +1,5 @@
 const aside = document.querySelector("aside");
+const menutoggle = document.querySelector(".menutoggle");
 aside.querySelector("button").addEventListener("click", (e) => {
   console.log(e.target);
   console.log(aside.classList);
@@ -6,5 +7,13 @@ aside.querySelector("button").addEventListener("click", (e) => {
     aside.classList.add("hidden");
     document.querySelector("main").style.maxWidth = "1920px";
     document.querySelector(".searchFormDiv").style.maxWidth = "1920px";
+    menutoggle.classList.remove("hidden");
   }
+});
+
+menutoggle.addEventListener("click", (e) => {
+  menutoggle.classList.add("hidden");
+  aside.classList.remove("hidden");
+  document.querySelector("main").style.maxWidth = "1470px";
+  document.querySelector(".searchFormDiv").style.maxWidth = "1470px";
 });
